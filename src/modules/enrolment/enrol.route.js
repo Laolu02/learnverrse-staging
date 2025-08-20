883702981b83103902850b3e41693fa55d9e904e
Micheal <michealobarewon2015@gmail.com>
2025-08-20 08:38:59 +0100

    enrol

--- src/modules/enrolment/enrol.route.js
+++ src/modules/enrolment/enrol.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { courseEnrollmentController } from './enrol.controller.js';
+import passport from 'passport';
 
 const router = express.Router();
 
-router.post('/enrol', courseEnrollmentController);
+router.post(
+  '/enrol',
+  passport.authenticate('jwt', { session: false }),
+  courseEnrollmentController
+);
 
 export default router;
